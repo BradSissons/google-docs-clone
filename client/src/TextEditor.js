1fc90bd6ed953e38980318d1ec8f19f10daeac4f
BradSissons <b.sissons98@gmail.com>
2021-08-28 15:52:20 +1200

    Added toolbar and print formatting

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -11,5 +11,5 @@ export default function TextEditor() {
         wrapper.append(editor)
         new Quill(editor, { theme: "snow" })
     }, [])
-    return <div id="container" ref={wrapperRef}></div>
+    return <div className="container" ref={wrapperRef}></div>
 }
